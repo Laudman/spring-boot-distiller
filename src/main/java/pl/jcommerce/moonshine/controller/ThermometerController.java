b8de93dd5661e729907af6f0a7338fdfd3d91835
Witold Pomazanka <mulaczos@gmail.com>
2016-06-15 09:09:59 +0200

    improve thermometer list appearance

--- src/main/java/pl/jcommerce/moonshine/controller/ThermometerController.java
+++ src/main/java/pl/jcommerce/moonshine/controller/ThermometerController.java
@@ -2,6 +2,7 @@ package pl.jcommerce.moonshine.controller;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
